2e704fd6654a34e35c7038c7cc2dbc6275d0fca7
Samantha Malamut <smalamut13@gmail.com>
2015-10-14 21:24:59 -0400

    initial cheatsheet commit

---
+++ increment.js
@@ -0,0 +1,58 @@
+alert("WARNING: JavaScript to follow!");
+
+
+// variables 
+var myName = "Samantha";
+var myAge = "22";
+var myFavoriteFood = "pizza";
+var soluiton = myName + myAge + myFavoriteFood;
+console.log(soluiton);
+
+var x = 750;
+var y = 1088;
+var solution = x - y;
+console.log(x / 63);
+console.log(y * 100);
+console.log(solution);
+
+console.log(75 * 98);
+
+// different data types:
+// this is a string
+"Hello World!";
+// this is a number
+5;
+// this is boolean
+true;
+// this is boolean
+23 > 1; 
+
+// these are arrays
+// an array full of strings
+["Chuck", "Blair", "Serena", "Nate", "Dan", "Vanessa"];
+
+// an array holding multiple data types 
+["Samantha", "13", "22", "monkey", "Ryan", "puppy"];
+
+// arrays stored in variables
+var names = ["Amber", "Amy", "Carly", "Sara", "Kendall", 88, "rabbit"];
+console.log(names);
+console.log(names.length);
+console.log(names[2]);
+
+// an array storing other arrays
+var fruits = ["apple", "orange"];
+var junk = ["ice cream", "brownies"];
+var food = [ ["apple", "orange"], ["ice cream", "brownies"] ]
+console.log(food[1] [1]);
+console.log(food[0] [1]);
+console.log(food[1] [0]);
+
+// objects
+var movie = {actor: "Brad Pitt", actress: "Mila Kunis"};
+console.log(movie["actress"]);
+
+var thursdayTV = {abc: "Scandal", cbs: "Blacklist"};
+console.log(thursdayTV["cbs"]);
+
+
